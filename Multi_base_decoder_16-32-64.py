beead8b9c718323a641d3270a1faa2f1ae47716a
Umer Aqeel <33192785+HydraUmer@users.noreply.github.com>
2021-05-03 19:29:07 +0500

    Update Multi_base_decoder_16-32-64.py

--- Multi_base_decoder_16-32-64.py
+++ Multi_base_decoder_16-32-64.py
@@ -1,21 +1,24 @@
 import base64
 
-#files
-f = open("encodedflag.txt","r")
+#file path
+f = open("encodedflag.txt","r") 
 flag = f.read()
 
 #print(flag)
 
 res = ''
 
-for i in range(0, 5):
+#base 16 
+for i in range(0, 5):      
     res = base64.b16decode(flag)
     flag = res
 
+#base 32
 for i in range(0, 5):
     res = base64.b32decode(flag)
     flag = res
 
+#base 64
 for i in range(0, 5):
     res = base64.b64decode(flag)
     flag = res
